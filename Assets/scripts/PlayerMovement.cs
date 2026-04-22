f88c651ff2489a69ddb8576ebbcbc569fcdedb72
JeperaltaP <jeperaltap@eafit.edu.co>
2026-04-22 11:52:15 -0500

    corrección movimiento invertido

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour 
 {
     public float velocidad = 5f;
     public Animator animator;
-
+    
     void Update()
+    
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+        
         Vector3 movimiento = new Vector3(x, 0f, z);
-
-        transform.Translate(movimiento * velocidad * Time.deltaTime, Space.World);
+        
+        transform.Translate(movimiento * velocidad * Time.deltaTime, Space.Self);
+        
         float speed = movimiento.magnitude;
         animator.SetFloat("speed", speed);
     }
