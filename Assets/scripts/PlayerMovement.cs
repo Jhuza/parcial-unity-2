643145ac7c950c8c91eb6cb80c50d2eec3038bdf
JeperaltaP <jeperaltap@eafit.edu.co>
2026-04-22 16:08:41 -0500

    rotacionPersonaje, desvinculacionCamara

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,7 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float velocidad = 5f;
     public Animator animator;
+<<<<<<< Updated upstream
     
+=======
+    public Transform modelo;
+    public float rotationSpeed = 10f;
+
+>>>>>>> Stashed changes
     void Update()
     
     {
@@ -12,9 +18,24 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         
         Vector3 movimiento = new Vector3(x, 0f, z);
+<<<<<<< Updated upstream
         
         transform.Translate(movimiento * velocidad * Time.deltaTime, Space.Self);
         
+=======
+
+        // Mover al jugador en coordenadas globales
+        transform.Translate(movimiento * velocidad * Time.deltaTime, Space.World);
+
+        // Rotar solo el modelo
+        if (movimiento != Vector3.zero)
+        {
+            Quaternion rotacionObjetivo = Quaternion.LookRotation(movimiento);
+            modelo.rotation = Quaternion.Slerp(modelo.rotation, rotacionObjetivo, rotationSpeed * Time.deltaTime);
+        }
+
+        // Animación
+>>>>>>> Stashed changes
         float speed = movimiento.magnitude;
         animator.SetFloat("speed", speed);
     }
