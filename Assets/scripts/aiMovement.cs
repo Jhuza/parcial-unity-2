dbb7d4093e4fa3120c7a870bbf6836bf6c124471
JeperaltaP <jeperaltap@eafit.edu.co>
2026-04-22 21:15:39 -0500

    enemyAnimationsFunctional

--- Assets/scripts/aiMovement.cs
+++ Assets/scripts/aiMovement.cs
@@ -5,27 +5,63 @@ public class aiMovement : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator animator;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+
+        if (animator != null)
+            animator.applyRootMotion = false;
+
+        Debug.Log("¿Está en NavMesh? " + agent.isOnNavMesh);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.destination = target.position;
+        if (agent == null || !agent.isOnNavMesh || target == null) return;
+
+        agent.SetDestination(target.position);
+
+        // Solo setea el float si el parámetro existe en el Animator
+        if (animator != null && AnimatorHasParameter("velocidad", animator))
+        {
+            float speed = agent.velocity.magnitude;
+            animator.SetFloat("velocidad", speed);
+        }
+    }
+
+    bool AnimatorHasParameter(string paramName, Animator anim)
+    {
+        foreach (AnimatorControllerParameter param in anim.parameters)
+        {
+            if (param.name == paramName) return true;
+        }
+        return false;
     }
 
     void OnTriggerEnter(Collider other)
-{
-    Debug.Log(other.name);
-    Debug.Log(other.tag);
-
-    if (other.CompareTag("Player"))
     {
-        Debug.Log("tocó al jugador, -10 segundos");
-        GameManager.instance.RestarTiempo(10f);     
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("tocó al jugador, -10 segundos");
+
+            if (animator != null) animator.SetBool("ataque", true);
+            agent.isStopped = true;
+
+            GameManager.instance.RestarTiempo(10f);
+        }
     }
-}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (animator != null) animator.SetBool("ataque", false);
+            agent.isStopped = false;
+        }
+    }
 }
